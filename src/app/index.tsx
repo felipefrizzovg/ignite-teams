838f6b9b4a08bf18f0afa09df978fb6ec6b8cd54
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-06-17 16:56:00 -0300

    fontes personalizadas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import theme from "@/src/theme";
+
 import Groups  from "@/src/components/Groups";
+import { ActivityIndicator } from "react-native";
+
 
 export default function Index() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -1,9 +1,9 @@
-import { Container, Title } from "./Groups.styles";
+import { Container } from "./Groups.styles";
 
 export default function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
+      
     </Container>
   );
 }
