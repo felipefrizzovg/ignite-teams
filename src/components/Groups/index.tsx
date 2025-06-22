4e0b780d788d6f245957036050312a929d6d007d
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-06-22 17:05:16 -0300

    Criando componente Highlight

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from "../Header";
+import { Highlight } from "../Highlight";
+
 import { Container } from "./Groups.styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
     </Container>
   );
 }
