4965ea25b463f524fbedb6c561147d11b1b51c35
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-06-23 15:23:20 -0300

    Estilizando o Placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,13 @@
 import { TextInputProps } from "react-native";
 import { Container } from "./Input.styles";
+import { useTheme } from "styled-components/native";
 
 export function Input({...rest}: TextInputProps) {
+  const { COLORS } = useTheme()
+
   return (
-    <Container 
+    <Container
+      placeholderTextColor={COLORS.GRAY_300}
       {...rest}
     />
   );
