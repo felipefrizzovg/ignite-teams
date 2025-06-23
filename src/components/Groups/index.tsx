8eeb29a5b0a187917bbeb32af53bf912fed40543
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-06-23 13:08:51 -0300

    Componente ListEmpty

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from "react-native";
 import { GroupCard } from "../GroupCard";
 import { Header } from "../Header";
 import { Highlight } from "../Highlight";
+import { ListEmpty } from "../ListEmpty";
 
 import { Container } from "./Groups.styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera do Ignite']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -26,6 +27,10 @@ export function Groups() {
           <GroupCard 
             title={item}  
             />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
       
