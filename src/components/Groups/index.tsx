b601d040cddd5d069d05dea4bb506364410c445a
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-06-26 16:05:00 -0300

    Abrindo turmas

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -9,11 +9,13 @@ import ListEmpty from "../ListEmpty";
 import { Container } from "./Groups.styles";
 import Button from "../Button";
 
-import { Link, useFocusEffect } from "expo-router";
+import { Link, useFocusEffect, useRouter } from "expo-router";
 import groupsGetAll from "@/src/storage/group/groupsGetAll";
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const router = useRouter();
 
   async function fetchGroups() {
     try {
@@ -22,6 +24,13 @@ export default function Groups() {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  function handleOpenGroup(group: string) {
+    router.push({
+      pathname: "/players",
+      params: { group },
+    });
   }
 
   useFocusEffect(
@@ -39,7 +48,9 @@ export default function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
